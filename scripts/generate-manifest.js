5c410a16e282abf9103a8a6e174b0c08c323b4cb
levin <xinyunzhang977@gmail.com>
2026-07-28 19:20:55 +0800

    fix: delete old icon PNGs, update manifest to use logo.svg

--- scripts/generate-manifest.js
+++ scripts/generate-manifest.js
@@ -25,24 +25,9 @@ const manifestTemplate = {
   background_color: '#000000',
   icons: [
     {
-      src: '/icons/icon-192x192.png',
-      sizes: '192x192',
-      type: 'image/png',
-    },
-    {
-      src: '/icons/icon-256x256.png',
-      sizes: '256x256',
-      type: 'image/png',
-    },
-    {
-      src: '/icons/icon-384x384.png',
-      sizes: '384x384',
-      type: 'image/png',
-    },
-    {
-      src: '/icons/icon-512x512.png',
+      src: '/logo.svg',
       sizes: '512x512',
-      type: 'image/png',
+      type: 'image/svg+xml',
     },
   ],
 };
